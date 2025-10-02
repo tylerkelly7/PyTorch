a05232b8f916fe63298bf1d060ac0e352e1fd1b7
Tyler Kelly <107662723+tylerkelly7@users.noreply.github.com>
2025-10-01 22:00:21 -0400

    Refactor models.py
    Change super definition and constructor instances. Incorporate OOP using classes and instances, encapsulate layers, separation of feature extractor and classifier. Make clear init/forward responsibilities.

--- src/models.py
+++ src/models.py
@@ -1,23 +1,34 @@
 import torch.nn as nn
 
 class SimpleCNN(nn.Module):
-    def __init__(self, num_classes=10):
-        super(SimpleCNN, self).__init__()
-        self.conv = nn.Sequential(
-        	nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+    """A minimal CNN for 28.28 grayscale images (MNIST)."""
+    
+    def __init__(self, num_classes: int=10):
+        """
+        __init__ is the constructor. It sets up layers and parameters.
+        `self` is the instance being created; it lets methods access attributes.
+        """
+        super().__init__()
+        self.features = nn.Sequential(
+        	nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1), #28x28 -> 28x28
         	nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(32, 64, kernel_size=3),
-            nn.ReUL(),
-            nn.MaxPool2d(2)
+        	nn.MaxPool2d(2),										#28x28 -> 14x14
+        	nn.Conv2d(32, 64, kernel_size=3),						#14x14 -> 12x12
+        	nn.ReUL(),
+        	nn.MaxPool2d(2)											#12x12 -> 6x6
         )
-        self.fc = nn.Sequential(
+        self.classifier = nn.Sequential(
         	nn.linear(64 * 6 * 6, 128),
-            nn.ReLU(),
-            nn.Linear(128, num_classes)
+        	nn.ReLU(),
+        	nn.Linear(128, num_classes)
         )
     
-    def forward(self, x):
-        x = self.conv(x)
-        x = x.view(x.size(0), -1)
-        return self.fc(x)
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        forward defines the computation graph.
+        PyTorch calls this inside __all__ and records ops for autograd
+        """
+    	x = self.features(x)
+    	x = x.view(x.size(0), -1)	# flatten N, C*H*W
+    	x = self.classifier(x)
+        return x
