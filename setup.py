5607b54d1c67906fc9236cacd54c2c75d4418f3d
Tyler Kelly <107662723+tylerkelly7@users.noreply.github.com>
2025-10-01 21:31:57 -0400

    Update setup.py
    Require python 3.10 or greater

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="PyTorch",
     version="0.1",
-    packages=find_packages(where="src"),
+    packages=find_packages("src"),
     package_dir={"": "src"},
+    python_requires=">=3.10"
 )
